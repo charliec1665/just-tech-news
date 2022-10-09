db936485b5937a2f0edb5749c4c17edf1cd9394d
Charlie Carter <charliec1665@gmail.com>
2022-10-09 15:51:03 -0400

    user create, read, update, and delete routes

---
+++ models/User.js
@@ -0,0 +1,62 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our User model
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+    {
+        // define an id column
+        id: {
+            // use the special sequelize datatypes object provide what type of data it is
+            type: DataTypes.INTEGER,
+            // this is the equivalent of SQL's 'NOT NULL' option
+            allowNull: false,
+            // instruct that this is the Primary Key
+            primaryKey: true,
+            // turn on auto increment
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define an email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // there cannot be any duplicate email values in this table
+            unique: true,
+            // if allowNull is set to false, we can run our data through validators before creating the table data
+            validate: {
+                isEmail: true
+            }
+        },
+        // define a password column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // this means the password must be at least four characters long
+                len: [4]
+            }
+        }
+    },
+    {
+        // TABLE CONFIGURATION OPTIONS
+
+        // pass in our imported sequelize connection (the direct connection to our database)
+        sequelize,
+        // dont automatically create createdAt/updatedAt timestamp fields
+        timestamps: false,
+        // dont pluralize name of database table
+        freezeTableName: true,
+        // use underscores instead of camel-casing
+        underscored: true,
+        // make it so our model name stays lowercase in the database
+        modeName: 'user'
+    }
+);
+
+module.exports = User;
