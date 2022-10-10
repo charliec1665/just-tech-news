e445d5e01c2624ca561b73a93205eb8f9260e5d5
Charlie Carter <charliec1665@gmail.com>
2022-10-10 15:04:28 -0400

    refactored code

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -78,33 +78,12 @@ router.post('/', (req, res) => {
 // PUT /api/posts/upvote
 // has to be defined before :id PUT, otherwise express will think 'upvote' is a valid parameter for id
 router.put('/upvote', (req, res) => {
-    Vote.create({
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-        .then(() => {
-            // then find the post we just voted on
-            return Post.findOne({
-                where: {
-                    id: req.body.post_id
-                },
-                attributes: [
-                    'id',
-                    'post_url',
-                    'title',
-                    'created_at',
-                    // use raw MySQL aggregate function query to get a count of how many votes post has, return under name 'vote_count'
-                    [
-                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-                        'vote_count'
-                    ]
-                ]
-            })
-            .then(dbPostData => res.json(dbPostData))
-            .catch(err => {
-                console.log(err);
-                res.status(400).json(err);
-            });
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+        .then(updatedPostData => res.json(updatedPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
         });
 });
 
